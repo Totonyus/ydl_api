e9a4a3aab276a45d399e84dfdaf682784e57a1fe
Totonyus <mail@totonyus.ovh>
2020-09-17 00:14:11 +0200

    Big changes for proofing presets.

--- main.py
+++ main.py
@@ -54,50 +54,120 @@ def check_download(url, format):
             return {'checked' : True, 'errors' : True}
 
 ### Launch the download instruction
-def launch_download(url, ydl_opts):
-    logging.info(f"Downloading '{url}' with quality '{ydl_opts['format']}' in '{ydl_opts['outtmpl']}'")
+def launch_downloads(url, downloads):
+    for download in downloads:
+        ydl_opts = download.get('ydl_opts')
+        logging.info(f"Downloading '{url}' with quality '{ydl_opts['format']}' in '{ydl_opts['outtmpl']}'")
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-@app.get("/download")
-async def create_download(response : Response, background_tasks : BackgroundTasks, url: str,
-                          format: str = params.default_format, subtitles : str = params.default_subtitles_languages, location: str = "default", filename : str = "default"):
-    decoded_url = unquote(url)
-    decoded_format = unquote(format)
-    decoded_subtitles = subtitles.split(',') if subtitles is not None else None
-
+# preset parameters are overide by query params
+def set_ydl_opts(definitive_params, url):
     # used to pass useful vars for naming purpose
     ydl_api_opts = {
-        'url': decoded_url,
-        'hostname' : urlparse(decoded_url).hostname,
-        'location_identifier' : location,
-        'filename_identifier' : filename
+        'hostname' : urlparse(url).hostname,
+        'location_identifier' : definitive_params.get('location'),
+        'filename_identifier' : definitive_params.get('filename'),
     }
 
+    logging.error(ydl_api_opts)
+
     download_dir = params.download_dir(ydl_api_opts)
+    name_template = params.file_name_template(ydl_api_opts)
 
     ydl_opts = {
         'quiet': True,
         'ignoreerrors' : True,
-        'outtmpl': download_dir + params.file_name_template(ydl_api_opts),
-        'format': decoded_format,
+        'subtitlesformat': params.default_subtitles_format,
         'noplaylist': params.no_playlist,
-        'writesubtitles': subtitles is not None,
-        'subtitleslangs' : decoded_subtitles,
-        'subtitlesformat': params.default_subtitles_format
+        'format': definitive_params.get('format'),
+        'subtitleslangs' : definitive_params.get('subtitles').split(',') if definitive_params.get('subtitles') is not None else None,
+        'writesubtitles' : definitive_params.get('subtitles') is not None,
+        'outtmpl' : download_dir.get('result') + name_template.get('result'),
+        'ydl_api_technical' : {'download_dir_default' : download_dir.get('default'), 'name_template_default' : name_template.get('default')}
     }
 
-    checked_download = check_download(url, decoded_format)
+    return ydl_opts
 
-    if checked_download['checked'] is False:
-        background_tasks.add_task(launch_download, decoded_url, ydl_opts)
-        response.status_code = 202
-    elif checked_download['checked'] and checked_download['errors'] is False:
-        background_tasks.add_task(launch_download, decoded_url, ydl_opts)
+def get_definitive_params(query_params, preset = None):
+    default_preset = params.get_presets_params('default').get('result')
+
+    if preset is None:
+        preset = default_preset
+
+    definitive_params = {}
+    for param in ['format', 'subtitles', 'location', 'filename']:
+        definitive_params[param] = query_params.get(param) if query_params.get(param) is not None else preset.get(param) if preset.get(param) is not None else default_preset.get(param)
+    return definitive_params
+
+@app.get("/download")
+async def create_download(response : Response, background_tasks : BackgroundTasks, url: str,
+                          format: str = None, subtitles : str = None, location: str = None, filename : str = None, presets: str = None):
+
+    selected_presets_objects = []
+    decoded_presets = []
+
+    if presets is not None:
+        decoded_presets = presets.split(',') if presets is not None else None
+        selected_presets_objects = existing_presets(decoded_presets)
+
+    decoded_url = unquote(url)
+
+    query_params = {
+        'format' : unquote(format) if format is not None else None,
+        'subtitles' : unquote(subtitles) if subtitles is not None else None,
+        'location' : unquote(location) if location is not None else None,
+        'filnema' : unquote(filename) if filename is not None else None,
+        'presets' : unquote(presets) if presets is not None else None
+    }
+
+    checked_downloads_list = []
+
+    if len(selected_presets_objects) == 0:
+        definitive_params = get_definitive_params(query_params, None)
+        ydl_opts = set_ydl_opts(definitive_params, decoded_url)
+        checked_downloads_list.append({'check_result' : check_download(url, definitive_params.get('format')), 'ydl_opts' : ydl_opts})
+    else:
+        for selected_preset in selected_presets_objects:
+            definitive_params = get_definitive_params(query_params, selected_preset)
+            ydl_opts = set_ydl_opts(definitive_params, decoded_url)
+            checked_downloads_list.append({'check_result' : check_download(url, definitive_params.get('format')), 'ydl_opts' : ydl_opts})
+
+    validity_check = are_all_downloads_valid(checked_downloads_list)
+
+    if validity_check.get('checked') == validity_check.get('total') and validity_check.get('errors') == 0:
+        background_tasks.add_task(launch_downloads, decoded_url, checked_downloads_list)
         response.status_code = 200
+    elif validity_check.get('checked') != validity_check.get('total'):
+        background_tasks.add_task(launch_downloads, decoded_url, checked_downloads_list)
+        response.status_code = 202
     else:
         logging.error(f"Impossible to download '{decoded_url}'")
         response.status_code = 400
 
-    return {'url' : decoded_url, 'format': decoded_format, 'download_dir' : download_dir, 'status' : response.status_code, 'subtitles' : decoded_subtitles, 'checked_download' : checked_download}
+    return {'url' : decoded_url, 'presets_errors' : (len(decoded_presets) - len(selected_presets_objects)), 'list' : checked_downloads_list}
+
+def are_all_downloads_valid(download_list):
+    checked_number = 0
+    errors_number = 0
+
+    for download in download_list:
+        if download.get('check_result').get('checked') : checked_number += 1
+        if download.get('check_result').get('errors') : errors_number += 1
+    return {'checked' : checked_number, 'errors' : errors_number, 'total' : len(download_list)}
+
+def existing_presets(presets):
+    existing = []
+    for preset in presets:
+        preset_object = params.get_presets_params(preset)
+        if preset_object is not None and preset_object.get('default') is False:
+            existing.append(preset_object.get('result'))
+
+    if len(existing) == 0:
+        existing.append(params.get_presets_params('default'))
+
+    return existing
+
+#TODO refactoring
+#TODO update doc
--- params.py
+++ params.py
@@ -21,26 +21,41 @@ playlist_detection = [
 ### Change here the download directory and the file name : https://github.com/ytdl-org/youtube-dl/tree/3e4cedf9e8cd3157df2457df7274d0c842421945#output-template
 # provided vars
 # ydl_api_opts = {'url', 'hostname', 'location_identifier' }
+root_dir = "."
+def download_dir(ydl_api_opts):
+    templates={ ###- --%--- REPLACE ---%--- here with your different download directorues
+        'default' : f"{root_dir}/{ydl_api_opts.get('hostname')}/",
+        'audio' : f"{root_dir}/audio/",
+        #'date' : f"{root_dir}{date.today().strftime('%Y_%m_%d')}/" # for example : &location=date
+    }
+    return get_value(templates, ydl_api_opts, 'location_identifier')
+
 def file_name_template(ydl_api_opts):
-    filename_template = {
+    templates = {
         'default' : "%(title)s_(%(height)s).%(ext)s",
         'audio' : "%(title)s_(%(vbr)s).%(ext)s",
     }
-    return get_value(filename_template, ydl_api_opts, 'filename_identifier')
+    return get_value(templates, ydl_api_opts, 'filename_identifier')
 
-# Multiple choices with the parameter &location
-root_dir = "/"
-def download_dir(ydl_api_opts):
-    locations={ ###- --%--- REPLACE ---%--- here with your different download directorues
-        'default' : f"{root_dir}{ydl_api_opts.get('hostname')}/",
-        #'date' : f"{root_dir}{date.today().strftime('%Y_%m_%d')}/" # for example : &location=date
+def get_presets_params(preset_name):
+    templates={
+        'default' : {'format' : default_format, 'subtitles' : default_subtitles_languages, 'location' : 'default', 'filename' : 'default'},
+        'audio': {'format' : 'bestaudio', 'subtitles' : default_subtitles_languages, 'location' : 'audio', 'filename' : 'audio'},
+        'best' : {'format' : 'bestvideo+bestaudio/best', 'subtitles' : default_subtitles_languages, 'location' : 'default', 'filename' : 'default'},
+        'fullhd' : {'format' : 'best[height=1080]/best', 'subtitles' : default_subtitles_languages, 'location' : 'default', 'filename' : 'default'},
+        'hd' : {'format' : 'best[height=720]/best', 'subtitles' : default_subtitles_languages, 'location' : 'default', 'filename' : 'default'},
+        'sd' : {'format' : 'best[height=360]/best', 'subtitles' : default_subtitles_languages, 'location' : 'default', 'filename' : 'default'},
     }
-    return get_value(locations, ydl_api_opts, 'location_identifier')
+
+    return get_value(templates, {'preset' : preset_name}, 'preset')
 
 def get_value(list, ydl_api_opts, entry_name):
+    default = False
+
     if list.get(ydl_api_opts.get(entry_name)) is not None:
         result = list.get(ydl_api_opts.get(entry_name))
     else:
         logging.warning(f"{entry_name} : {ydl_api_opts.get(entry_name)} identifier not found. Using the default one instead")
         result = list.get('default')
-    return result
+        default = True
+    return {'result' : result, 'default' : default}
